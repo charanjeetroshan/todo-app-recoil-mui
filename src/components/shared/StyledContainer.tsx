32724d0193f3c0214217441abed6db83d77beebc
Charanjeet <singhcharanjeet603@gmail.com>
2024-06-27 16:48:19 +0200

    Add a utility container component

---
+++ src/components/shared/StyledContainer.tsx
@@ -0,0 +1,9 @@
+import { Container, styled } from "@mui/material";
+
+export const StyledContainer = styled(Container)({
+  width: "100%",
+  maxWidth: "1280px",
+  paddingBlock: "2rem",
+  margin: "0 auto",
+  textAlign: "center",
+});
